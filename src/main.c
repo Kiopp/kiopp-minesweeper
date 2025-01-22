7846f9cf37fca5e7f52f5908747e6ee0091f7259
Jesper Wentzell <jesper.wentzell@gmail.com>
2025-01-22 14:53:01 +0100

    Game win-condition
    - Board is generated when the user presses the start button.
    - Window size is dynamic and depends on the size of the board, so
      no tiles should render out of bounds.
    - User will win the game when all empty tiles are explored.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 enum gameState {
     s_setup,
     s_playing,
+    s_win,
     s_game_over
 };
 
@@ -17,6 +18,7 @@ int main()
     InitWindow(screen_width, screen_height, "Kiopp Minesweeper");
     SetTargetFPS(60);
 
+
     // Game setup
     enum gameState state = s_setup;
     char btn_text[32] = "Start";
@@ -26,17 +28,7 @@ int main()
     Texture2D tilesheet = LoadTexture("../assets/kiopp_minesweeper_tilesheet.png");
     TileMapTexture textures = SplitTileMap(tilesheet);
 
-    GameGrid grid = CreateGrid(
-        screen_width, 
-        screen_height, 
-        tilesheet.width/2, 
-        &textures, 
-        16, 
-        16, 
-        48, 
-        2, 
-        25
-        );
+    GameGrid grid;
 
 
     int flag_enable = 0;
@@ -46,27 +38,79 @@ int main()
         switch (state) {
             case s_setup:
                 HandleTextButtonPress(&button);
+
+                if (button.button_pressed) {
+                    // Grid settings
+                    int tile_size = tilesheet.width/2;
+                    int grid_cols = 9;
+                    int grid_rows = 9;
+                    int num_mines = 2;
+                    int scale = 4;
+                    int tile_font = 25;
+
+                    // Handle button press
+                    button.button_pressed = 0;
+                    state = s_playing;
+
+                    // Change window size
+                    int grid_width = grid_cols * tile_size * scale;
+                    int grid_height = grid_rows * tile_size * scale;
+
+                    screen_width = grid_width + 200; // grid_width + padding
+                    screen_height = grid_height + 200; // grid_height + padding
+
+                    SetWindowSize(screen_width, screen_height);
+
+                    // Generate grid
+                    grid = CreateGrid(
+                        screen_width, 
+                        screen_height, 
+                        tile_size, 
+                        &textures, 
+                        grid_cols, 
+                        grid_rows, 
+                        num_mines, 
+                        scale, 
+                        tile_font
+                        );
+                }
                 break;
+
             case s_playing:
+                // Handle flag keybind
                 if (IsKeyDown(KEY_LEFT_SHIFT)) { 
                     flag_enable = 1; 
                 } else {
                     flag_enable = 0;
                 }
+
+                // Handle tiles
                 HandleGridTileButtons(&grid);
                 HandleGridTileButtonClicked(&grid, &textures, flag_enable);
+
+                // Check game over condition
+                if (grid.game_over == 1) {
+                    state = s_game_over;
+                }
+                if (grid.game_win == 1) {
+                    state = s_win;
+                } 
+                
                 break;
+
             case s_game_over:
                 break;
+
+            case s_win:
+                break;
+
             default:
                 break;
         }
         
         
     /*
-        Have a keyboard shortcut for placing flags left shift has to be held down and is not a toggle.
-
-        As well as a toggle button that can be toggled by clicking on it with the mouse.
+        Add toggle button that can be toggled by clicking on it with the mouse.
     */
 
         BeginDrawing();
@@ -76,22 +120,24 @@ int main()
         {
             case s_setup:
                 DrawTextButton(&button);
-                if (button.button_pressed) {
-                    button.button_pressed = 0;
-                    state = s_playing;
-                }
                 break;
+
             case s_playing:
                 DrawGameGrid(&grid);
-                if (grid.game_over == 1) {
-                    state = s_game_over;
-                }
                 break;
+
+            case s_win:
+                DrawGameGrid(&grid);
+                DrawCircle(screen_width/2, screen_height/2, 100, GREEN);
+                break;
+
             case s_game_over:
                 DrawGameGrid(&grid);
                 DrawCircle(screen_width/2, screen_height/2, 100, RED);
+                break;
+
             default:
-                DrawCircle(screen_width/2, screen_height/2, 100, RED);
+                DrawCircle(screen_width/2, screen_height/2, 100, BLUE);
                 break;
         }
         EndDrawing(); 
